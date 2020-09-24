b72a2fb2c8204fe31f186742b2568742bc59cb8c
Diarrah <diarrahcasiano@gmail.com>
2020-09-24 14:19:13 -0700

    i tags self-closing

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,35 +1,37 @@
 import React from 'react';
+import { Button, Links } from './';
 import logoWhite from '../images/logo-white.svg';
-import {Button, Links} from './';
 
 const Footer = () => {
     return (
         <div className="footer">
             <img className="footer__logo" src={logoWhite} alt="Easybank company logo" />
+
             <div className="footer__social__logos">
                 <a href="#" aria-label="Facebook icon">
-                    <i className="fab fa-facebook-square"></i>
+                    <i className="fab fa-facebook-square" />
                 </a>
                 <a href="#" aria-label="YouTube icon">
-                    <i className="fab fa-youtube-square"></i>
+                    <i className="fab fa-youtube-square" />
                 </a>
                 <a href="#" aria-label="Twitter icon">
-                    <i className="fab fa-twitter"></i>
+                    <i className="fab fa-twitter" />
                 </a>
                 <a href="#" aria-label="Pinterest icon">
-                    <i className="fab fa-pinterest"></i>
+                    <i className="fab fa-pinterest" />
                 </a>
                 <a href="#" aria-label="Instagram icon">
-                    <i className="fab fa-instagram"></i>
+                    <i className="fab fa-instagram" />
                 </a>
             </div>
 
             <div className="footer__links">
                 <Links/>
             </div>
-            <Button/>
-            <p className="footer__copyright">©Easybank. All Rights Reserved</p>
 
+            <Button/>
+            
+            <p className="footer__copyright">©Easybank. All Rights Reserved</p>
         </div>
     )
 }
